62d5d640f365885076109acb2d4c9a91b7d4559b
Srikrishna Sakunia <sakuniacoolkrishna636@gmail.com>
2024-08-03 20:29:06 +0530

    Camera Screen Created

--- app/src/main/java/dev/krishna/imagecolourextractor/navigation/NavGraph.kt
+++ app/src/main/java/dev/krishna/imagecolourextractor/navigation/NavGraph.kt
@@ -1,21 +1,37 @@
 package dev.krishna.imagecolourextractor.navigation
 
+import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import dev.krishna.imagecolourextractor.presentation.DetailsScreen
 import dev.krishna.imagecolourextractor.presentation.HomeScreen
+import dev.krishna.imagecolourextractor.presentation.camera.CameraScreen
 
 @Composable
-fun MyNavGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = "Screen1") {
-        composable("Screen1") {
-            HomeScreen(navController)
+fun MyNavGraph(
+    navController: NavHostController,
+    applicationContext: Context
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screens.Home.route
+    ) {
+        composable(Screens.Home.route) {
+            HomeScreen (
+                moveToDetails = { navController.navigate(Screens.Details.route) }
+            )
         }
-        composable("details_screen_route") {
-            DetailsScreen(navController = navController)
+        composable(Screens.Details.route) {
+            DetailsScreen (
+                moveToCamera = { navController.navigate(Screens.CameraScreen.route) }
+            )
+        }
+        composable(Screens.CameraScreen.route) {
+            CameraScreen (
+                applicationContext = applicationContext
+            )
         }
     }
-    
 }
